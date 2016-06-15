5fcc9f831d8709847540935907bc656ef255779b
EK <ek@r2s-MacBook-Pro.local>
2016-06-15 13:07:10 -0700

    first commit

---
+++ js/init.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  "use strict";
+
+  // Preloader
+  $( window ).load(function() {
+      $("#loading-img").fadeOut();
+      $("#preloader").delay(400).fadeOut("slow");
+  });
+
+  $(document).ready(function(){
+
+    // Create launch date for ticker
+    // Date below denotes 23 April, 2015
+    $(function () {
+      var launchDay = new Date(2016, 8-1, 30);
+      $('#ticker').countdown({
+        until: launchDay
+      });
+    });
+
+    // SVG Fallback for older browsers
+    $(function() {
+      if (!Modernizr.svg) {
+        $(".logo img").attr('src', function(index, attr) {
+          return attr.replace('svg', 'png');
+        });
+      }
+    });
+
+    // Placeholder initialise
+    $('input, textarea').placeholder();
+
+  });
+
+});
